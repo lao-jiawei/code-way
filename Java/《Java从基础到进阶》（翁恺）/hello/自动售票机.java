3c93f8e36c6af0d4741ecbe6da6d81406f6a877f
Enj0y2001 <72645902+Enj0y2001@users.noreply.github.com>
2021-05-31 17:11:39 +0800

    Rename Java/《Java从基础到进阶》（翁恺）/自动售票机.java to Java/《Java从基础到进阶》（翁恺）/hello/自动售票机.java

--- Java/《Java从基础到进阶》（翁恺）/hello/自动售票机.java
+++ Java/《Java从基础到进阶》（翁恺）/hello/自动售票机.java
@@ -1,29 +1,29 @@
 package hello;
 import java.util.Scanner;
 
-public class ×Ô¶¯ÊÛÆ±»ú {
-	//	×Ô¶¯ÊÛÆ±»ú
+public class è‡ªåŠ¨å”®ç¥¨æœº {
+	//	è‡ªåŠ¨å”®ç¥¨æœº
 	public static void main(String[] args) {
-		//³õÊ¼»¯
+		//åˆå§‹åŒ–
 		Scanner in =new Scanner(System.in);
 		int balance=0;
-		//	¶ÁÈëÍ¶±Ò½ğ¶î
+		//	è¯»å…¥æŠ•å¸é‡‘é¢
 		while(true)
 		{
-			System.out.println("ÇëÍ¶±Ò£º");
+			System.out.println("è¯·æŠ•å¸ï¼š");
 			int amount = in.nextInt();
 			balance=balance+amount;
 			System.out.println(amount);
 			if(balance>=10)
 			{
-				//	´òÓ¡³µÆ±
+				//	æ‰“å°è½¦ç¥¨
 				System.out.println("********************");
-				System.out.println("* Java³Ç¼ÊÌúÂ·×¨Ïß *");
-				System.out.println("*   ÎŞÖ¸¶¨×ùÎ»Æ±   *");
-				System.out.println("*   Æ±¼Û£º 10Ôª    *");
+				System.out.println("* JavaåŸé™…é“è·¯ä¸“çº¿ *");
+				System.out.println("*   æ— æŒ‡å®šåº§ä½ç¥¨   *");
+				System.out.println("*   ç¥¨ä»·ï¼š 10å…ƒ    *");
 				System.out.println("********************");
-				//	¼ÆËã²¢´òÓ¡ÕÒÁã
-				System.out.println("ÕÒÁã£º"+(amount-10));
+				//	è®¡ç®—å¹¶æ‰“å°æ‰¾é›¶
+				System.out.println("æ‰¾é›¶ï¼š"+(amount-10));
 				balance=0;
 			}
 			else 
