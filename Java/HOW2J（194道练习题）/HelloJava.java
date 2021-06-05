af6394cf58113a2cba461963d1ecfedbbf05e0c2
Enj0y2001 <72645902+Enj0y2001@users.noreply.github.com>
2021-06-05 17:26:50 +0800

    Add files via upload

---
+++ Java/HOW2J（194道练习题）/HelloJava.java
@@ -0,0 +1,9 @@
+package JAVA»ù´¡;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
---
+++ Java/HOW2J（194道练习题）/Item.java
@@ -0,0 +1,22 @@
+package JAVA基础;
+
+public class Item {
+	private String name;
+	private int price;
+	public Item(String objName,int objprice) 
+	{
+		name=objName;
+		price=objprice;
+	}
+	
+	public static void main(String[] args) {
+		Item item1=new Item("血瓶",50);
+		Item item2=new Item("草鞋",300);
+		Item item3=new Item("长剑",350);
+		
+		System.out.println("名称\t价格");
+		System.out.println(item1.name+"\t"+item1.price);
+		System.out.println(item2.name+"\t"+item2.price);
+		System.out.println(item3.name+"\t"+item3.price);
+		}
+}
